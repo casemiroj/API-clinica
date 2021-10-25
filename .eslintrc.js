942987e16f68ffad9e803c5a98d0c5be1e0e7fc9
João Casemiro <jv.casemiro@icloud.com>
2021-10-25 20:11:21 -0300

    Iniciando projeto

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
