ef460672616e2a75403ed3fd5a6891a81f5f0ea9
João Casemiro <jv.casemiro@icloud.com>
2021-11-11 20:38:04 -0300

    Add custom css

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const swaggerUi = require('swagger-ui-express');
 const ConsultaController = require('./app/controllers/ConsultaController');
 const swaggerDocument = require('./swagger.json');
 
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: 'Clinica UNI9',
+};
+
 const router = Router();
 
 router.post('/consultas', ConsultaController.criarConsulta);
@@ -11,6 +16,6 @@ router.get('/consultas/:id', ConsultaController.listarConsultaPorId);
 router.put('/consultas/:id', ConsultaController.atualizarConsulta);
 router.patch('/consultas/retorno/:id', ConsultaController.retornoConsulta);
 router.delete('/consultas/:id', ConsultaController.deleteConsulta);
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 module.exports = router;
