66573025808df6dfd1bfa3eba79288dcac1c6900
João Casemiro <jv.casemiro@icloud.com>
2021-11-11 20:21:33 -0300

    Documentando api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
 const router = require('./routes');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 app.use(express.json());
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@uninove-clinica.uqrsa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
